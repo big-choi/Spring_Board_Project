70b0d303d9cf54dc41e9845c255a3c697073e7cd
bos3321 <bos33210@naver.com>
2021-09-20 23:54:04 +0900

    이메일 인증번호 발송 기능

--- bigchoi_Board/src/main/java/com/bigchoi/controller/MemberController.java
+++ bigchoi_Board/src/main/java/com/bigchoi/controller/MemberController.java
@@ -1,7 +1,12 @@
 package com.bigchoi.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Random;
 
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +26,9 @@ public class MemberController {
 
 	@Autowired
 	private MemberService memberService;
+	
+	@Autowired
+	private JavaMailSender mailSender;
 
 	// 회원가입
 	@RequestMapping(value = "/join", method = RequestMethod.POST)
@@ -54,18 +62,61 @@ public class MemberController {
 	@RequestMapping(value = "/memberIdChk", method = RequestMethod.POST)
 	@ResponseBody
 	public String memberIdChkPOST(String id) throws Exception {
-		
+
 		log.info("memberIdChk() 진입");
-		
+
 		int result = memberService.idCheck(id);
-		
+
 		log.info("결과값 : " + result);
-		
-		if(result != 0) {
-			return "fail"; //중복 아이디 존재
+
+		if (result != 0) {
+			return "fail"; // 중복 아이디 존재
 		} else {
-			return "success"; //중복 아이디 없음
+			return "success"; // 중복 아이디 없음
 		}
 
 	} // memberIdChkPOST() 종료
+
+	/* 이메일 인증 */
+	@RequestMapping(value = "/mailCheck", method = RequestMethod.GET)
+	@ResponseBody
+	public String mailCheckGET(String email) throws Exception {
+
+		/* 뷰(View)로부터 넘어온 데이터 확인 */
+		log.info("이메일 데이터 전송 확인");
+		log.info("인증번호 : " + email);
+		
+		/* 인증번호 생성*/
+		Random random = new Random();
+		int num = random.nextInt(888888)+100000;
+		log.info("인증번호 "+ num);
+		
+		/* 이메일 보내기 */
+        String from = "bos33210@naver.com";
+        String to = email;
+        String title = "회원가입 인증 이메일 입니다.";
+        String content = "인증 번호는 " + num + " 입니다.";
+        
+        try {
+            
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "utf-8");
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(title);
+            helper.setText(content,true);
+            mailSender.send(message);
+            
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        
+        //View로 다시 반환할 때 데이터 타입은 String만 가능
+       
+        String stringNumber = Integer.toString(num);
+        
+        return stringNumber;
+		
+				
+	}
 }
